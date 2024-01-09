2579c187297198e8a05887b3d5c79d4289d1f15f
Tobias Løfgren <tobloef@gmail.com>
2024-01-09 08:46:52 +0100

    wip additional deep types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export * from './deep-omit';
+export * from './deep-types/deep-omit';
+export * from './deep-types/deep-pick';
+export {IsNever} from "./util-types/is-never";
+export {IsArray} from "./util-types/is-array";
